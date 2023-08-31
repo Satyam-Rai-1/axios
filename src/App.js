8b122278653716b1db59f703e3b248cf243dc33f
Satyam-Rai-1 <satyam154gzp@gmail.com>
2023-08-31 22:27:28 +0530

    Get Data Using Axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+
+  const [myData,setMyData] = useState([]);
+
+  const [isError,setIsError] = useState("")
+
+  // using Promises
+
+  // useEffect(()=>{
+
+  //   axios.get("https://jsonplaceholder.typicode.com/posts")
+  //   .then((res) => setMyData(res.data))
+  //   .catch((error)=>setIsError(error.message));
+
+  // },[])
+
+
+  //using Async Await
+
+  const getApiData = async() =>{
+  try{
+    
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts")
+      setMyData(res.data)
+    }
+  
+    catch(error)
+    {
+      setIsError(error.message);
+    }
+  }
+  useEffect(() =>{
+    getApiData();
+
+  },[])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>    
+    
+    <h1>Axios</h1>
+
+    {isError !== "" && <h2> {isError}</h2>}
+    <div className="grid">
+      {myData.slice(0,12).map((post)=>{
+        const {id,title,body} = post;
+        return <div className="card" key={id}>
+          <h2>{title.slice(0,15).toUpperCase() }</h2>
+          <p>{body.slice(0,150)}</p>
+        </div>
+      })}
     </div>
+    </>
+
   );
 }
 
